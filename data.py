da7e162b68235ff940ee67245b0ec51befc27a76
AngelFelipeMP <angel.magnossao@gmail.com>
2022-04-12 16:06:28 +0200

    "adapt data-process fuc to exist data"

--- data.py
+++ data.py
@@ -1,17 +1,13 @@
 import config 
-from utils import download_data, process_OSACT2022_data
+from utils import download_data, process_EXIST2022_data
 
 if __name__ == "__main__":
     # download_data(config.DATA_PATH,
     #                 config.DATA_URL
     # )
     
-    
-    process_OSACT2022_data(config.DATA_PATH, 
-                    config.DATASET_COLUMNS, 
-                    config.DATASET_TEXT, 
+    process_EXIST2022_data(config.DATA_PATH, 
                     config.DATASET_CLASSES, 
-                    config.DATASET_INDEX,
-                    config.USEFUL_COLUMNS
+                    config.DATASET_INDEX
     )
 
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import pandas as pd
 import gdown
-from preprocess import ArabertPreprocessor
+# from preprocess import ArabertPreprocessor
 import config
 
 def download_data(data_path, data_urls):
@@ -25,40 +25,34 @@ def download_data(data_path, data_urls):
         shutil.rmtree(sorce_folder)
         
 
-
-def process_OSACT2022_data(data_path, header, text_col, labels_col, index_col, columns_to_read):
-    arabic_prep = ArabertPreprocessor("aubmindlab/bert-base-arabertv2", keep_emojis = True)
+def process_EXIST2022_data(data_path, labels_col, index_col):
     files = [f for f in os.listdir(data_path) if 'processed' not in f]
     
     for file in files:
-        if 'test' not in file:
-            df = pd.read_csv(data_path + '/' + file, sep='\t', header=None, usecols=columns_to_read)
-        else:
-            df = pd.read_csv(data_path + '/' + file, sep='\t', header=None, usecols=[0,1])
-        
+        df = pd.read_csv(data_path + '/' + file)
         print(df)
         
         if 'train' in file or 'dev' in file:
-            df[df.shape[1]+1] = df.iloc[:,-1].apply(lambda x: x if x == 'NOT_HS' else 'HS')
-            df = df[df.columns.tolist()[:-2] + df.columns.tolist()[-1:] + df.columns.tolist()[-2:-1]]
-            df.columns = header
+            # df[df.shape[1]+1] = df.iloc[:,-1].apply(lambda x: x if x == 'NOT_HS' else 'HS')
+            # df = df[df.columns.tolist()[:-2] + df.columns.tolist()[-1:] + df.columns.tolist()[-2:-1]]
+            # df.columns = header
             df.replace(labels_col, inplace=True)
             print(df.head())
-        else:
-            df.columns = header[:-3]
-            print('@'*20)
-            print(header[:-3])
+        # else:
+        #     # df.columns = header[:-3]
+        #     print('@'*20)
+        #     print(header[:-3])
 
-        text_col_processed = text_col + '_processed'
-        pass_value_config('DATASET_TEXT_PROCESSED', '\'' +  text_col_processed + '\'')
-        df[text_col_processed] = df.loc[:, text_col].apply(lambda x: arabic_prep.preprocess(x))
-        print(df.head())
+        # text_col_processed = text_col + '_processed'
+        # pass_value_config('DATASET_TEXT_PROCESSED', '\'' +  text_col + '\'')
+        # df[text_col_processed] = df.loc[:, text_col].apply(lambda x: arabic_prep.preprocess(x))
+        # print(df.head())
         
-        dataset_name =  file[:-4] + '_processed' + '.txt'
+        dataset_name =  file[:-4] + '_processed' + '.csv'
         variable = 'DATASET' + ['_TRAIN' if 'train' in file else '_DEV' if 'dev' in file else '_TEST'][0]
         pass_value_config(variable, '\'' + dataset_name + '\'')
         
-        df.to_csv(data_path + '/' + dataset_name, index=False, sep='\t',  index_label=index_col)
+        df.to_csv(data_path + '/' + dataset_name, index=False,  index_label=index_col)
 
 
 def pass_value_config(variable, value):
@@ -74,3 +68,40 @@ def map_labels(df, labels_col):
     for col, labels in labels_col.items():
         df.replace({col:{number: string for string, number in labels.items()}}, inplace=True)
     return df
+
+
+
+
+# def process_OSACT2022_data(data_path, header, text_col, labels_col, index_col, columns_to_read):
+#     arabic_prep = ArabertPreprocessor("aubmindlab/bert-base-arabertv2", keep_emojis = True)
+#     files = [f for f in os.listdir(data_path) if 'processed' not in f]
+    
+#     for file in files:
+#         if 'test' not in file:
+#             df = pd.read_csv(data_path + '/' + file, sep='\t', header=None, usecols=columns_to_read)
+#         else:
+#             df = pd.read_csv(data_path + '/' + file, sep='\t', header=None, usecols=[0,1])
+        
+#         print(df)
+        
+#         if 'train' in file or 'dev' in file:
+#             df[df.shape[1]+1] = df.iloc[:,-1].apply(lambda x: x if x == 'NOT_HS' else 'HS')
+#             df = df[df.columns.tolist()[:-2] + df.columns.tolist()[-1:] + df.columns.tolist()[-2:-1]]
+#             df.columns = header
+#             df.replace(labels_col, inplace=True)
+#             print(df.head())
+#         else:
+#             df.columns = header[:-3]
+#             print('@'*20)
+#             print(header[:-3])
+
+#         text_col_processed = text_col + '_processed'
+#         pass_value_config('DATASET_TEXT_PROCESSED', '\'' +  text_col_processed + '\'')
+#         df[text_col_processed] = df.loc[:, text_col].apply(lambda x: arabic_prep.preprocess(x))
+#         print(df.head())
+        
+#         dataset_name =  file[:-4] + '_processed' + '.txt'
+#         variable = 'DATASET' + ['_TRAIN' if 'train' in file else '_DEV' if 'dev' in file else '_TEST'][0]
+#         pass_value_config(variable, '\'' + dataset_name + '\'')
+        
+#         df.to_csv(data_path + '/' + dataset_name, index=False, sep='\t',  index_label=index_col)
