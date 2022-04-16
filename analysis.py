491c631ca1d508f0b6ead09dd385f84eb706cbfc
AngelFelipeMP <angel.magnossao@gmail.com>
2022-04-16 11:37:26 +0200

    "update analyses for multilingual data"

--- analysis.py
+++ analysis.py
@@ -7,22 +7,19 @@ import config
 if __name__ == "__main__":
     df_train = pd.read_csv(config.DATA_PATH + '/' + config.DATASET_TRAIN)
     df_dev = pd.read_csv(config.DATA_PATH + '/' + config.DATASET_DEV)
-    df = pd.concat([df_train, df_dev]).reset_index(drop=True)
+    df_all = pd.concat([df_train, df_dev]).reset_index(drop=True)
+
 
     for language in config.TRANSFORMERS.keys():
         for transformer in config.TRANSFORMERS[language]:
+            
+            if language!='text':
+                df = df_all.loc[(df_all['language']==language), df_all.columns]
         
-            print(f'Transformer: {transformer} \n')
-
+            print(f'\nTransformer: {transformer} - Language/Text: {language}')
             tokenizer = AutoTokenizer.from_pretrained(transformer)
 
-            if language == 'en':
-                df['tokens'] = df['English'].apply(lambda x: tokenizer.tokenize(x))
-            elif language == 'es':
-                df['tokens'] = df['Spanish'].apply(lambda x: tokenizer.tokenize(x))
-            else:
-                df['tokens'] = df['text'].apply(lambda x: tokenizer.tokenize(x))
-            
+            df['tokens'] = df[language].apply(lambda x: tokenizer.tokenize(x))
             df['number_tokens'] = df['tokens'].apply(lambda x: len(x))
 
             ax = df['number_tokens'].plot.hist(bins=20, range=(0, 512))
@@ -33,3 +30,4 @@ if __name__ == "__main__":
             print('Porcentage of comments under 128 tokens: ',((df.query('number_tokens < 128').size / df.size) * 100))
             print('Porcentage of comments under 256 tokens: ',((df.query('number_tokens < 256').size / df.size) * 100))
             print('Porcentage of comments under 512 tokens: ',((df.query('number_tokens < 512').size / df.size) * 100))
+            
